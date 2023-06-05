958ca1e16a65a2fec20aac4505c8580501c14fac
Muchammad Dzanur Ragil Putra <d24nur@gmail.com>
2023-06-05 15:24:48 +0700

    asd

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Aplikasi ambil tikor online, Aplikasi tikor online, get latlong, get latitude, get longitude, online">
     <title>Get TiKor Online</title>
+    <link rel="icon" type="image/x-icon" href="/assets/img/favicon.ico">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="style.css">
@@ -24,7 +26,17 @@
             <div class="card-body">
             <h5 class="card-title mb-3">Ambil Titik koordinat</h5>
             <div id="map-canvas" class="mb-3"></div>
-            <form onLoad="initGeolocation();" id="tampilkan" NAME="rd" METHOD="POST">
+            <?php
+            if(!isset($_POST['latlong'])) {
+            echo "";
+            }else {
+            ?>
+            <div id="alert" class="alert alert-success alert-dismissible fade show" role="alert">
+                Sukses : <?= $_POST['latlong']?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <?php }?>
+            <form id="tampilkan" NAME="rd" METHOD="POST">
                 <div class="form-group">
                     <label for="lat">Latitude</label>
                     <INPUT TYPE="text" NAME="lat" ID="lat" class="form-control" VALUE="" disabled>
@@ -33,14 +45,15 @@
                     <label for="long">Longitude</label>
                     <INPUT TYPE="text" NAME="long" ID="long" class="form-control" VALUE="" disabled>
                 </div>
-                <div class="form-group">
-                  <label class="" for="inlineFormInput">Posisi</label>
-                  <div class="input-group mb-3">
-                      <!-- <input type="text" class="form-control mb-2" id="latlong" name="latlong"> -->
-                      <input type="text" class="form-control" id="latlong" name="latlong" aria-describedby="basic-addon1">
-                      <button class="input-group-text bg-primary text-light border-grey" id="basic-addon1" onclick="copy_text()" onmouseout="outFunc()"><span class="tooltiptext" id="myTooltip"><i class="fa-regular fa-clipboard"></i></span></button>
-                  </div>
-                </div>
+                <form action="index.php" method="post">
+                    <div class="form-group">
+                    <label class="" for="inlineFormInput">Posisi</label>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" id="latlong" name="latlong" aria-describedby="basic-addon1">
+                        <button class="input-group-text bg-primary text-light border-grey" id="basic-addon1" onclick="copy_text()" onmouseout="outFunc()"><span class="tooltiptext" id="myTooltip"><i class="fa-regular fa-clipboard"></i></span></button>
+                    </div>
+                    </div>
+                </form>
                 <div class="d-grid gap-2">
                     <button type="button" onclick="getLocation(); showlocation()" class="btn btn-primary"><i class="fa-solid fa-location-dot text-danger shadow"></i> Ambil lokasi</button>
                 </div>
